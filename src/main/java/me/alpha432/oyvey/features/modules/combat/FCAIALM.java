9ed918af64aaac2b311bbc21bb1b5e8938740c15
LachCrafter <lachcrafteryt@gmail.com>
2024-08-04 22:49:36 +0200

    future beta

--- src/main/java/me/alpha432/oyvey/features/modules/combat/FCAIALM.java
+++ src/main/java/me/alpha432/oyvey/features/modules/combat/FCAIALM.java
@@ -12,21 +12,28 @@ import net.minecraft.util.EnumHand;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import com.mojang.realmsclient.gui.ChatFormatting;
 
+import java.util.Random;
+
 public class FCAIALM extends Module {
     private static FCAIALM INSTANCE = new FCAIALM();
-    enum CAMode {None, AP, One, Default}
+    enum CAMode {None, AP, One, Beta, Default}
     enum SurroundMode {AntiDie, AutoObsidian, Surround, Default}
     Setting<CAMode> camode = register(new Setting("CA", CAMode.Default));
     Setting<SurroundMode> surroundmode = register(new Setting("Surround", SurroundMode.Default));
     Setting<Boolean> doWatermark = register(new Setting("Watermark", false));
     Setting<String> cWatermark = register(new Setting("W", "Future v2.11.1", v -> doWatermark.getValue()));
     Setting<Boolean> noinfo = register(new Setting("NoInfo", false));
+    Setting<Boolean> futurebeta = register(new Setting("Future Beta", false));
     boolean APstringAttacking = true;
     boolean APhasPlaced = false;
     boolean APhasAttacked = false;
     boolean APpacketAttacking = true;
     boolean APnothing = true;
     Timer APtimer = new Timer();
+    private Random rand = new Random();
+    private Timer updateTimer = new Timer();
+
+    private String firstNumber = "0", secondNumber = "0.0", thirdNumber = "0";
 
     public FCAIALM() {
         super("FCAIALM", "very cool", Category.COMBAT, true, false, false);
@@ -86,53 +93,70 @@ public class FCAIALM extends Module {
     public String replaceWithAliases(String text) {
         if (!isOn()) return text;
 
-        if (text.contains(" " + ChatFormatting.GRAY + "[") && noinfo.getValue()) {
-            return text.replaceAll(" " + ChatFormatting.GRAY + "\\[.*]", "");
+        // Update the random values every 500ms (half a second)
+        if (updateTimer.passedMs(500)) {
+            if (APhasAttacked) {
+                firstNumber = String.valueOf(rand.nextInt(100));
+            } else {
+                firstNumber = "0";
+            }
+            if (APhasPlaced) {
+                secondNumber = String.format("%.1f", rand.nextDouble());
+                thirdNumber = String.valueOf(rand.nextInt(100));
+            } else {
+                secondNumber = "0.0";
+                thirdNumber = "0";
+            }
+            updateTimer.reset();
         }
 
         if (text.startsWith("AutoCrystal " + ChatFormatting.GRAY + "[")) {
-
             switch (camode.getValue()) {
-                case None: {
+                case None:
                     return "AutoCrystal";
-                }
-                case AP: {
+                case AP:
                     if (APnothing) {
                         return "AutoCrystal";
                     } else {
                         return "AutoCrystal " + ChatFormatting.GRAY + "[" + ChatFormatting.WHITE + (APstringAttacking ? "Attack" : "Place") + ChatFormatting.GRAY + "]";
                     }
-                }
-                case One: {
+                case One:
                     return text.replaceAll("\\,.*]", "]");
-                }
-                default: {
+                case Beta:
+                    // Only generate latency when APhasAttacked is true
+                    String latency = APhasAttacked ? String.format("%.2fms", rand.nextDouble() + 0.1) : "0.00ms";
+                    return "AutoCrystal " + ChatFormatting.GRAY + "[" + ChatFormatting.WHITE + latency + ChatFormatting.GRAY + ", " + ChatFormatting.WHITE + firstNumber + ChatFormatting.GRAY + ", " + ChatFormatting.WHITE + secondNumber + ChatFormatting.GRAY + ", " + ChatFormatting.WHITE  + thirdNumber + ChatFormatting.GRAY + "]";
+                default:
                     return text;
-                }
             }
         }
 
         if (text.startsWith("FeetTrap " + ChatFormatting.GRAY + "[")) {
             switch (surroundmode.getValue()) {
-                case AntiDie: {
+                case AntiDie:
                     return "AntiDie";
-                }
-                case AutoObsidian: {
+                case AutoObsidian:
                     return "AutoObsidian";
-                }
-                case Surround: {
-                    return "Surround";
-                }
-                default: {
+                case Surround:
+                    return "FeetTrap " + ChatFormatting.GRAY + "[" + ChatFormatting.WHITE + "0" + ChatFormatting.GRAY + "]";
+                default:
                     return text;
-                }
             }
+        }
+
+        if (text.startsWith("AutoTrap " + ChatFormatting.GRAY + "[")) {
+            return "AutoTrap " + ChatFormatting.GRAY + "[" + ChatFormatting.WHITE + "0" + ChatFormatting.GRAY + "]";
+        }
+
+
+        if (text.startsWith("Future v") && futurebeta.getValue()) {
+            return "Future v2.13.5+27.68af4a4971";
         }
 
         if (text.startsWith("Future v") && doWatermark.getValue()) {
             return cWatermark.getValue();
         }
-        
+
         return text;
     }
 
